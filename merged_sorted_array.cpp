65e98be00c508af3b1d94d986a8047a03c4bb2ed
hari kommi <naraharikommi644@gmail.com>
2023-01-22 18:38:55 +0530

    first done

---
+++ merged_sorted_array.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1,j=n-1,k=m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k] = nums1[i];
+                i--;
+                k--;
+            }else{
+                nums1[k] = nums2[j];
+                j--;
+                k--;
+            }
+        }
+        while(j>=0){
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+        
+    }
+int main(){
+    int m,n;
+    cin>>m>>n;
+    vector<int> nums1(m);
+    vector<int> nums2(n);
+    for(int i=0;i<m;i++){
+        cin>>nums1[i];
+    }
+    for(int i=0;i<n;i++){
+        cin>>nums2[i];
+    }
+    merge(nums1,m,nums2,n);
+    return 0;
+}
